d066a7792e7330153d556fa44dbb781c3cbb33b8
Minsik97 <instant2100@naver.com>
2020-11-11 17:11:56 +0900

    practice

---
+++ src/com/javaex/practice/Ex01.java
@@ -0,0 +1,82 @@
+package com.javaex.practice;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		
+		//Ex01
+		boolean var = true;
+		int of_workers = 1;
+
+		//Ex02
+		System.out.println(12/5-1);
+		System.out.println(5+19%3);
+		
+		//Ex03
+		int x = 1;
+		int y = 1;
+		
+		int a = ++x *2;
+		int b = y++ *2;
+		
+		System.out.println("a=" + a);
+		System.out.println("b=" + b);
+		System.out.println("x=" + x);
+		System.out.println("y=" + y);
+		
+		//Ex04
+		x = 10;
+		y = 20;
+		
+		int sum = x+y;
+		
+		System.out.println("합은" + sum);
+		
+		//Ex05
+		double speed = 90.0;
+		double time = 60.0;
+		double distance = speed * time;
+		System.out.println(distance);
+		
+		//E06
+		int i = 10;
+		int n = i++ %2;
+		
+		System.out.println(i);
+		System.out.println(n);
+		
+		//Ex07
+		int q = 10;
+		int w = ++q %2;
+		
+		System.out.println(q);
+		System.out.println(w);
+		
+		//Ex08
+		int e,r = 0;
+		char grad = 'A';
+		String salarty = "2,000,000";
+		short t = 1000;
+		
+		//Ex09
+		double f = 80.0;
+		System.out.println(5/9*(f-32.0));
+		
+		//Ex10
+		int o = 10;
+		System.out.println(o);
+		
+		//Ex12
+		final double PI = 3.14;
+		double nn = 28.26;
+		System.out.println(nn*(PI*PI));
+		
+		//Ex13
+		final double dal = 1230.95;
+		System.out.println(123096/dal);
+		System.out.println(1000000/dal);
+		
+		
+	}
+
+}
